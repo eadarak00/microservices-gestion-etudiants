1652ee70d15f4cc6ddf92cf3b014a9ece118bcc2
eadarak00 <eadarak2001@gmail.com>
2025-12-17 19:14:28 +0000

    feat: enable config server functionality

--- microservices/config-server/src/main/java/sn/uasz/m2Info/config_server/ConfigServerApplication.java
+++ microservices/config-server/src/main/java/sn/uasz/m2Info/config_server/ConfigServerApplication.java
@@ -2,8 +2,10 @@ package sn.uasz.m2Info.config_server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
+@EnableConfigServer
 public class ConfigServerApplication {
 
 	public static void main(String[] args) {
