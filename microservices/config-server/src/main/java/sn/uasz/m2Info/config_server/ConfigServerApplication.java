6af159b2595381af31b3d6add2561c0084ba3488
eadarak00 <eadarak2001@gmail.com>
2025-12-17 18:39:57 +0000

    feat: Initialize the Spring Cloud Config Server application.

---
+++ microservices/config-server/src/main/java/sn/uasz/m2Info/config_server/ConfigServerApplication.java
@@ -0,0 +1,13 @@
+package sn.uasz.m2Info.config_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigServerApplication.class, args);
+	}
+
+}
