12cee78668446936b18bc80d9f104c4a02fff44e
eadarak00 <eadarak2001@gmail.com>
2025-12-29 17:50:57 +0000

    feat: Initialisation du microservice etudiant-service avec configuration Maven, classes principales et tests

---
+++ microservices/etudiant-service/src/test/java/sn/uasz/m2info/etudiant_service/EtudiantServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package sn.uasz.m2info.etudiant_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EtudiantServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
