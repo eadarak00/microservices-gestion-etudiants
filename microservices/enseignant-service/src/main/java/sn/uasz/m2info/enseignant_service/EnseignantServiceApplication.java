e8f9db643b12553dc57abd61d73c575bbea96283
eadarak00 <eadarak2001@gmail.com>
2025-12-31 16:41:31 +0000

    feat: Ajouter le service enseignant avec la configuration Maven, les fichiers de démarrage et les tests de contexte

---
+++ microservices/enseignant-service/src/main/java/sn/uasz/m2info/enseignant_service/EnseignantServiceApplication.java
@@ -0,0 +1,13 @@
+package sn.uasz.m2info.enseignant_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnseignantServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnseignantServiceApplication.class, args);
+	}
+
+}
---
+++ microservices/enseignant-service/src/test/java/sn/uasz/m2info/enseignant_service/EnseignantServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package sn.uasz.m2info.enseignant_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnseignantServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
