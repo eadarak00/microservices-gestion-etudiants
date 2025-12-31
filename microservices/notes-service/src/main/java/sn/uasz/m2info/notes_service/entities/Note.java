cdabfdb77b1a81c3765283b850f1f1c707a15728
eadarak00 <eadarak2001@gmail.com>
2025-12-31 22:15:03 +0000

    feat: Ajouter l'entité Note pour gérer les notes des étudiants

---
+++ microservices/notes-service/src/main/java/sn/uasz/m2info/notes_service/entities/Note.java
@@ -0,0 +1,36 @@
+package sn.uasz.m2info.notes_service.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "notes")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Note {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Long etudiantId;
+
+    private Double valeur;
+
+    @ManyToOne
+    @JoinColumn(name = "evaluation_id")
+    private Evaluation evaluation;
+}
