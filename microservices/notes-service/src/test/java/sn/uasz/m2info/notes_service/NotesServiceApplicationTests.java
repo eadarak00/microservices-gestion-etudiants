521ec610d02e3a3a43316b56ea598d0b66d22198
eadarak00 <eadarak2001@gmail.com>
2025-12-31 21:42:48 +0000

    feat: Initialiser le service de gestion des notes avec Maven Wrapper, configuration de l'application et tests

---
+++ microservices/notes-service/src/test/java/sn/uasz/m2info/notes_service/NotesServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package sn.uasz.m2info.notes_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotesServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
