9ff1020338e7a6d7edda1599895bb44dab761ea3
eadarak00 <eadarak2001@gmail.com>
2025-12-31 21:50:46 +0000

    feat: Ajouter l'entité Evaluation pour gérer les évaluations des étudiants

---
+++ microservices/notes-service/src/main/java/sn/uasz/m2info/notes_service/entities/Evaluation.java
@@ -0,0 +1,32 @@
+package sn.uasz.m2info.notes_service.entities;
+
+import java.time.LocalDate;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Evaluation {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String type;
+    private LocalDate date;
+
+    private Long classeId;
+    private Long matiereId;
+}
