e550dcf500f9064fb40ec63ff6f0db1da2a839c4
eadarak00 <eadarak2001@gmail.com>
2025-12-31 16:51:15 +0000

    feat: Ajouter l'entité Affectation pour gérer les associations entre enseignants, classes et matières

---
+++ microservices/enseignant-service/src/main/java/sn/uasz/m2info/enseignant_service/entities/Affectation.java
@@ -0,0 +1,31 @@
+package sn.uasz.m2info.enseignant_service.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Affectation {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Long classeId;
+    private Long matiereId;
+
+    @ManyToOne
+    private Enseignant enseignant;
+}
