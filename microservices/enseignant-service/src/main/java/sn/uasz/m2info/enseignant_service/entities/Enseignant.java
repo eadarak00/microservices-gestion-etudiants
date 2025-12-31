3f429134be28091a0e207f85ea775cf887a60caf
eadarak00 <eadarak2001@gmail.com>
2025-12-31 16:41:58 +0000

    feat: Ajouter l'entité Enseignant avec ses attributs et annotations JPA

---
+++ microservices/enseignant-service/src/main/java/sn/uasz/m2info/enseignant_service/entities/Enseignant.java
@@ -0,0 +1,40 @@
+package sn.uasz.m2info.enseignant_service.entities;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Pattern;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Enseignant {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String matricule;
+
+    private String nom;
+
+    private String prenom;
+
+    private String email;
+
+    private String telephone;
+
+    private String specialite;
+}
