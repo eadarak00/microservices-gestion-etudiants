5a948f29f01972a3c9fd8615cb55889ded4a7d13
eadarak00 <eadarak2001@gmail.com>
2026-01-13 22:22:45 +0000

    feat: Mise en place du service d'inscription

---
+++ frontend/src/services/inscription.service.ts
@@ -0,0 +1,12 @@
+import api from "../api/axios";
+import type { Inscription, InscriptionCreate } from "../types/inscription";
+
+
+const BASE_URL = "/inscriptions";
+
+
+export const createInscription = (data: InscriptionCreate) =>
+  api.post<Inscription>(BASE_URL, data);
+
+export const getAllInscriptionEtudiants = (etudiantId : number) => 
+    api.get<Inscription>(`${BASE_URL}/etudiant/${etudiantId}`)
