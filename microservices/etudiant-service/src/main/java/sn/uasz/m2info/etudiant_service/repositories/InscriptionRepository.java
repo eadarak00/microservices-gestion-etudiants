d4a83562ecaca41e7e159b0fcab095fcff3529e9
eadarak00 <eadarak2001@gmail.com>
2026-01-14 22:11:03 +0000

    feat: methode pour modifier les inscriptions

--- microservices/etudiant-service/src/main/java/sn/uasz/m2info/etudiant_service/repositories/InscriptionRepository.java
+++ microservices/etudiant-service/src/main/java/sn/uasz/m2info/etudiant_service/repositories/InscriptionRepository.java
@@ -4,6 +4,9 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import sn.uasz.m2info.etudiant_service.entities.EtatInscription;
@@ -14,15 +17,28 @@ public interface InscriptionRepository
         extends JpaRepository<Inscription, Long> {
 
     List<Inscription> findByClasseId(Long classeId);
-    List<Inscription>  findByClasseIdAndEtat(Long classeId, EtatInscription etat);
+
+    List<Inscription> findByClasseIdAndEtat(Long classeId, EtatInscription etat);
 
     List<Inscription> findByEtudiantId(Long etudiantId);
 
     Optional<Inscription> findByEtudiantIdAndClasseId(Long etudiantId, Long classeId);
-    
+
     // Ajouter cette méthode
     boolean existsByEtudiantIdAndEtat(Long etudiantId, EtatInscription etat);
 
     Optional<Inscription> findByEtudiantIdAndEtat(Long etudiantId, EtatInscription etat);
+
+    @Modifying
+    @Query("""
+               UPDATE Inscription i
+               SET i.etat = :etat
+               WHERE i.etudiant.id = :etudiantId
+                 AND i.id <> :inscriptionId
+            """)
+    void updateAutresInscriptionsEtat(
+            @Param("etudiantId") Long etudiantId,
+            @Param("inscriptionId") Long inscriptionId,
+            @Param("etat") EtatInscription etat);
 
 }
